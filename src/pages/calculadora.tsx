bf5ea2dae6f8d5fa5ba8572a2317639e420697bf
Diego Rodrigo da Silva <diegorodrigo90@gmail.com>
2021-08-17 19:16:38 -0300

    max input length on calculator

--- src/pages/calculadora.tsx
+++ src/pages/calculadora.tsx
@@ -23,6 +23,7 @@ const Calcutator = () => {
             prefix="R$ "
             defaultValue={initialValue}
             decimalsLimit={2}
+            maxLength={15}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             onValueChange={value => {
               if (value) setInitialValue(parseFloat(value.replace(",", ".")));
